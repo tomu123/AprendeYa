e94872ff26ef6d9c51d4f00c6c12774290e19691
Isabel Tello <isabeltellohuaripata@gmail.com>
2022-02-08 09:45:25 -0500

    c

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,7 +7,7 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: %i[first_name last_name avatar])
-    devise_parameter_sanitizer.permit(:account_update, keys: %i[first_name last_name avatar])
+    devise_parameter_sanitizer.permit(:sign_up, keys: %i[first_name last_name avatar teacher])
+    devise_parameter_sanitizer.permit(:account_update, keys: %i[first_name last_name avatar teacher])
   end
 end
